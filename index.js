9844bb8161bf1240eef88f782f50e1bf8be1e7d3
Kreat1vK0dr <daniel@projectcodex.co>
2016-04-12 02:55:30 +0200

    enhanced layout and navigation.

--- index.js
+++ index.js
@@ -28,14 +28,31 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res){
+  app.engine('handlebars', exphbs({defaultLayout: 'main'}));
   res.redirect("/home");
 });
 
 app.get('/home', function(req,res) {
   res.render("home.handlebars");
 });
+app.get('/stats', function(req,res) {
+  app.engine('handlebars', exphbs({defaultLayout: 'stats'}));
+  res.render("stats_home.handlebars");
+});
+app.get('/summary', function(req,res) {
+  app.engine('handlebars', exphbs({defaultLayout: 'summary'}));
+  res.render("summary_home.handlebars");
+});
 
-app.get('/sales/:item_type/:week', function (req, res) {
+app.get('/stats/:item_type/:week', function (req, res) {
+  var type = req.params.item_type;
+  var week = req.params.week;
+  var data = display.getData(type, week);
+  var template = display.getTmplName(type);
+    res.render(template, data);
+});
+
+app.get('/summary/:item_type/:week', function(req,res){
   var type = req.params.item_type;
   var week = req.params.week;
   var data = display.getData(type, week);
