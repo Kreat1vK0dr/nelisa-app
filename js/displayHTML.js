8e298ef3e5b28e17d3bb229bbed5d052865059c3
Kreat1vK0dr <daniel@projectcodex.co>
2016-04-12 10:32:36 +0200

    changed appearance of tables and some other css stuff.

--- js/displayHTML.js
+++ js/displayHTML.js
@@ -22,7 +22,7 @@ exports.getHTML = function(type, week) {
                       dataProd.data.push(p);
                     }
                     console.log(dataProd);
-                    dataProd.title = "Summary by Product Week "+(n);
+                    dataProd.title = "Summary by Product for Week "+(n);
                     src = fs.readFileSync('./templates/summary_products.handlebars', 'utf8');
                     var tmpl = handlebars.compile(src);
                     console.log(tmpl(dataProd));
@@ -32,7 +32,7 @@ exports.getHTML = function(type, week) {
                     for (var c of groupC[n-1]) {
                       dataCat.data.push(c);
                     }
-                    dataCat.title = "Summary by Category Week "+(n);
+                    dataCat.title = "Summary by Category for Week "+(n);
                     src = fs.readFileSync('./templates/summary_categories.handlebars', 'utf8');
                     var tmpl = handlebars.compile(src);
                     return tmpl(dataCat);
@@ -47,14 +47,14 @@ exports.getData = function(type, week) {
   var n = week, src, tmpl;
   switch(type) {
     case "m&l" :
-                    dataML[n-1].title = "Most & Least Week "+(n);
+                    dataML[n-1].title = "Trend stats Week "+(n);
                     return dataML[n-1];
     case "product" :
                     var dataProd = {data: []};
                     for (var p of groupP[n-1]) {
                       dataProd.data.push(p);
                     }
-                    dataProd.title = "Summary by Product Week "+(n)
+                    dataProd.title = "Summary by Product for Week "+(n)
                     return dataProd;
     case "category" :
                     var dataCat = {data: []};
