13911d617459d73e196549a9c74ac449e523959e
Kreat1vK0dr <daniel@projectcodex.co>
2016-07-08 00:19:26 +0200

    made changes to map and markers on about page.

--- public/js/maps.js
+++ public/js/maps.js
@@ -45,17 +45,18 @@ var directionsDisplay, directionsService, mapDiv, map, geocoder, start, end;
 function initMap() {
     directionsService = new google.maps.DirectionsService();
     directionsDisplay = new google.maps.DirectionsRenderer();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var latlng = new google.maps.LatLng(-34.038141, 18.676916);
 
     mapDiv = document.getElementById('map');
 
     map = new google.maps.Map(mapDiv, {
-        mapTypeId: google.maps.MapTypeId.HYBRID,
+        // mapTypeId: google.maps.MapTypeId.HYBRID,
         center: latlng, // OR center: {lat: , lng: },
         zoom: 8
     });
 
     directionsDisplay.setMap(map);
+     directionsDisplay.setPanel(document.getElementById('right-panel'));
 
     var button = document.getElementById('address');
 
@@ -64,11 +65,34 @@ function initMap() {
             var start = new google.maps.LatLng(-33.9068394, 18.4185398);
             var end = new google.maps.LatLng(-34.038141, 18.676916);
 
-            var marker = new google.maps.Marker({
-                map: map,
-                position: start
-            });
+            var contentString = '<div id="content">'+
+                       '<div id="siteNotice">'+
+                       '</div>'+
+                       '<h1 id="firstHeading" class="firstHeading"><span id="nelisa-info-window">Nelisa\'s</span></h1>'+
+                       '<div id="bodyContent">'+
+                       '<p>A traditional necessity shop located in Khayelitsha. <br/>'+
+                       'Run by veteran <b>Nelisa</b> for the last 20 years, '+
+                       'you can be sure to be greeted with a warm smile '+
+                       'and enthusiastic conversation. </b> '+
+                       'It\'s not just your run of the mill spaza, it\'s a <b>truly South African</b> '+
+                       'feel-at-home <b>experience</b>.'
+                       '</div>'+
+                       '</div>';
 
+                   var infowindow = new google.maps.InfoWindow({
+                     content: contentString,
+                     maxWidth: 200
+                   });
+
+            var spazaMarker = new google.maps.Marker({
+                map: map,
+                position: end,
+                icon: "/images/spaza.png",
+                title: "Nelisa's Spaza"
+            });
+            spazaMarker.addListener('click', function() {
+                     infowindow.open(map, spazaMarker);
+                   });
             var request = {
                 origin: start,
                 destination: end,
