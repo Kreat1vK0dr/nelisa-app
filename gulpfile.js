bc78aeba51def3537955f4cd624329608712a3c0
Kreat1vK0dr <daniel@projectcodex.co>
2016-07-14 15:20:27 +0200

    added gulp functionality to minimise css and js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
-var gulp = require("gulp"),
-    mocha = require("gulp-mocha"),
-    shell = require("gulp-shell");
+    var gulp = require('gulp'),
+        sourcemaps = require('gulp-sourcemaps'),
+        concat = require('gulp-concat'),
+        uglify = require('gulp-uglify'),
+        sass = require('gulp-sass'),
+        cleanCSS = require('gulp-clean-css'),
+        autoprefixer = require('gulp-autoprefixer'),
+        gzip = require('gulp-gzip'),
+        imagemin = require('gulp-imagemin'),
+        rename = require('gulp-rename'),
+        nodemon = require('gulp-nodemon'),
+        watch = require('gulp-watch'),
+        mocha = require("gulp-mocha"),
+        shell = require("gulp-shell");
+
 
 gulp.task('db_setup', function(cb){
   shell.task([
@@ -15,3 +27,106 @@ cb();
   		.pipe(mocha());
       process.exit(0);
   });
+
+  // MUST RUN $ sudo gulp nodemon
+
+
+  // Optimize Images task
+  // gulp.task('compress-images', function () {
+  //     return gulp.src('./public/assets/img/**/*.{gif,jpg,png}')
+  //         .pipe(imagemin({
+  //             progressive: true,
+  //             interlaced: true,
+  //             svgoPlugins: [{
+  //                 removeViewBox: false
+  //             }, {
+  //                 removeUselessStrokeAndFill: false
+  //             }]
+  //         }))
+  //         .pipe(gulp.dest('./public/assets/img'))
+  // });
+
+  //OR THIS
+  // gulp.task('bundle-sass', function () {
+  //     gulp.src('./dev/sass/main.sass')
+  //         .pipe(sourcemaps.init())
+  //         .pipe(sass({
+  //             includePaths: ['./dev/sass'],
+  //             indentedSyntax: true,
+  //             outputStyle: 'expanded',
+  //             data: 'file'
+  //         }).on('error', sass.logError))
+  //         .pipe(sourcemaps.write('./maps'))
+  //         .pipe(gulp.dest('./public/css'));
+  // });
+
+  gulp.task('minify-css', function () {
+      gulp.src('./public/css/main.css')
+          .pipe(sourcemaps.init())
+          .pipe(autoprefixer({
+              browsers: ['last 2 versions'],
+              cascade: false
+          }))
+          .pipe(cleanCSS({
+              compatibility: 'ie8'
+          }))
+          .pipe(rename('main.min.css'))
+          .pipe(sourcemaps.write('./maps'))
+          .pipe(gulp.dest('./public/css'));
+  });
+
+  gulp.task('compress-css', function () {
+      gulp.src('./public/css/main.min.css')
+          .pipe(sourcemaps.init())
+          .pipe(gzip())
+          .pipe(sourcemaps.write('./maps'))
+          .pipe(gulp.dest('./public/css'));
+  });
+  //
+  // gulp.task('bundle-scripts', function () {
+  //     gulp.src('./dev/js/*.js')
+  //         .pipe(sourcemaps.init())
+  //         .pipe(concat('main.js'))
+  //         .pipe(sourcemaps.write('./maps'))
+  //         .pipe(gulp.dest('./public/js'));
+  // });
+
+  gulp.task('minify-js', function () {
+      gulp.src('./public/js/main.js')
+          .pipe(sourcemaps.init())
+          .pipe(uglify())
+          .pipe(rename('main.min.js'))
+          .pipe(sourcemaps.write('./maps'))
+          .pipe(gulp.dest('./public/js'));
+  });
+
+  gulp.task('compress-js', function () {
+      gulp.src('./public/js/main.min.js')
+          .pipe(sourcemaps.init())
+          .pipe(gzip())
+          .pipe(sourcemaps.write('./maps'))
+          .pipe(gulp.dest('./public/js'));
+  });
+
+  gulp.task('watch', function () {
+      gulp.watch('./dev/sass/*.sass', ['minify-css', 'compress-css']);
+      // gulp.watch('./dev/sass/*.sass', ['bundle-sass', 'minify-css', 'compress-css']);
+      gulp.watch('./dev/js/*.js', ['minify-js', 'compress-js']);
+      // gulp.watch('./dev/js/*.js', ['bundle-scripts', 'minify-js', 'compress-js']);
+      // gulp.watch('./public/assets/images/**/*.{gif,jpg,png}', ['compress-images']);
+  });
+
+  gulp.task('nodemon', function () {
+      nodemon({
+              script: 'app.js'
+          })
+          .on('start', ['minify-css', 'compress-css', 'minify-js', 'compress-js', 'watch'], function () {
+              console.log('start!');
+          })
+          .on('change', ['watch'], function () {
+              console.log('changed!');
+          })
+          .on('restart', function () {
+              console.log('restarted!');
+          });
+  })
